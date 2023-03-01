994b9f126e513781e811144ecb5f8f82be76df3f
CongBinhm <cbl091003@gmail.com>
2023-03-01 14:21:05 +0700

    complete update

--- controller/users/getUser.js
+++ controller/users/getUser.js
@@ -1,6 +1,7 @@
+const formatUserDataResponse = require("../../services/formatUserDataResponse");
 const getUser = async (req, res) => {
   try {
-    res.json({ data: req.user });
+    res.json({ data: formatUserDataResponse(req.user) });
   } catch (err) {
     res.status(401).json({ error: "Unauthorized" });
   }
